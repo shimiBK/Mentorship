bbc6c844b924711b580a6d936db40a2311fa215a
Shimmy BK <shimi.sbk@gmail.com>
2023-02-23 19:17:19 +0200

    update cors

--- api/server/index.js
+++ api/server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://livecody.onrender.com","https://code4moveo.onrender.com"],
+  origin: ["http://localhost:3000", "https://mentorship-api.onrender.com"]
 }
 
 
